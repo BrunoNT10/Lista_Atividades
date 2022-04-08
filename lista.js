2012300fce824b904322f97d2f9c1f63cecf8572
bruno1604 <brunont1211@gmail.com>
2022-04-08 14:29:00 -0300

    Editar tasks concluído

--- lista.js
+++ lista.js
@@ -1,18 +1,42 @@
+
 function DeleteItem(){
     console.log(this.parentElement)
     this.parentElement.remove()
 }
-function EditItem(lista){
+function EditItem(){
     let FatherElement = this.parentElement
     console.log(FatherElement)
-    console.log(FatherElement.children)
     
-    FatherElement.children[0].remove()
-    FatherElement.children[1].remove()
-    FatherElement.children[2].remove()
+    FatherElement.innerHTML = '';
+    const novo_item_lista = document.createElement('input');
+     
+    FatherElement.appendChild(novo_item_lista)
 
-    const inserir_input = document.createElement("input")
-    FatherElement.appendChild(inserir_input)
+    FatherElement.appendChild(CriarBotaoEdit())
+    
+    FatherElement.appendChild(CriarBotaoConcluido()) 
+    
+}
+function ConcluidoItem(){
+    var input_element = this.parentElement.children[0]
+    console.log(input_element.value)
+    console.log(this.parentElement)
+    lista = this.parentElement
+    this.parentElement.innerHTML = input_element.value
+
+    lista.appendChild(CriarBotaoDelete())
+
+    lista.appendChild(CriarBotaoEdit())
+}
+
+function CriarBotaoConcluido(){
+    const botao_concluido =  document.createElement('button')
+    botao_concluido.classList.add("btn", "btn-success")
+    botao_concluido.innerHTML = "Concluido"
+    botao_concluido.type = "button"
+    botao_concluido.addEventListener("click", ConcluidoItem);
+
+    return botao_concluido
 }
 function CriarBotaoDelete(){
     const botao_delete =  document.createElement('button')
@@ -35,6 +59,7 @@ function CriarBotaoEdit(){
 
 function Submit(){
     const lista = document.querySelector('[data-task]')
+    
     const valor = document.querySelector('[data-form-input]')
 
     const novo_item_lista = document.createElement("li")
